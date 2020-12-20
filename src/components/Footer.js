1f9f969b2644693b750a8623959f04cac6a3e14f
Andy Camann <acamann@gmail.com>
2020-12-20 14:37:23 -0600

    add facebook link to footer, clarify attribution

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      Follow us on{' '}
+      <a href="https://www.facebook.com/LittleLightsHouston/">Facebook</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
+          © Copyright {new Date().getFullYear()} All rights reserved. Yellowcake starter theme by{' '}
           <a href="https://thriveweb.com.au/">Thrive</a>.
         </span>
       </div>
